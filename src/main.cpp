83d73bf1cab327cfca9ca13d4a321a6b27c0dbc4
Zurich Hentley <zurichentley@gmail.com>
2023-02-26 20:40:22 -0500

    just some notes

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@
 #include "env.h"
 
 //need JSON objects to read the JSON coming from server for get
-#define LED_PIN0 2
-#define LED_PIN1 3
+#define LED_PIN0 2 //not sure which pins to use since i cant test physically
+#define LED_PIN1 23
 #define LED_PIN2 4
 
 void setup() {
@@ -14,7 +14,7 @@ void setup() {
   pinMode(LED_PIN1,OUTPUT);
   pinMode(LED_PIN2,OUTPUT);
 
-  char * endpoint = "https: //ecse-three-led-api.onrender.com";
+  char * endpoint ="https://ecse-three-led-api.onrender.com";
 
   Serial.begin(9600);
   WiFi.begin(WIFI_SSID, WIFI_PASS); // initiates connection
@@ -33,6 +33,7 @@ void loop() {
 if (WiFi.status() != WL_CONNECTED ){
   HTTPClient http;
   http.begin(endpoint);
+  http.addHeader("X-API-KEY","Flora#5798");
   String http_response;
   int httpResponseCode = http.GET();
 
@@ -49,20 +50,39 @@ if (WiFi.status() != WL_CONNECTED ){
     Serial.print(httpResponseCode);
   }
   http.end();
+  StaticJsonDocument<1024> doc;
+  DeserializationError error = deserializeJson(doc,http_response);
 
- digitalWrite(LED_PIN0,HIGH);
-    delay(500);
+  if (error){
+    Serial.print("Deserialize failed: ");
+    Serial.print(error.c_str());
+    return;
+  }
+
+  const bool LED0_state= doc["light0"];
+  const bool LED1_state= doc["light1"];
+  const bool LED2_state= doc["light2"];
+if(LED0_state==1){
+  digitalWrite(LED_PIN0,HIGH);
+    delay(100);
     digitalWrite(LED_PIN0,LOW);
-    delay(500);
-
-  digitalWrite(LED_PIN1,HIGH);
-    delay(500);
-    digitalWrite(LED_PIN1,LOW);
-    delay(500);
-
-  digitalWrite(LED_PIN2,HIGH);
-    delay(500);
-    digitalWrite(LED_PIN2,LOW);
-    delay(500);
+    delay(100);
 }
+if(LED1_state==1){
+  digitalWrite(LED_PIN1,HIGH);
+    delay(100);
+    digitalWrite(LED_PIN1,LOW);
+    delay(100);
+
+}
+if(LED2_state==1){
+  digitalWrite(LED_PIN2,HIGH);
+    delay(100);
+    digitalWrite(LED_PIN2,LOW);
+    delay(100);
+}
+}
+else return;
+  
+  
 }
